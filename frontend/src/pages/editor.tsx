8e399638664c83b0c8a6a069e3f9204999c8181e
ichwan haryo sembodo <ichwanharyosembodo96@gmail.com>
2025-02-17 18:01:39 +0900

    feat: working comments using local storage

--- frontend/src/pages/editor.tsx
+++ frontend/src/pages/editor.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AISidebar from "../components/aiSidebar";
 import PlateEditor, { InitiatePlateEditor } from "../components/textEditor/plate-editor";
 
 const EditorPage: React.FC = () => {
-    const editor = InitiatePlateEditor();
+    const STORAGE_KEY = 'editor-content';
+    const loadInitialValue = () => {
+        const savedValue = localStorage.getItem(STORAGE_KEY);
+        if (savedValue) {
+            return JSON.parse(savedValue);
+        }
+
+        // Default content if nothing is saved
+        return [
+            {
+                id: "1",
+                type: "p",
+                children: [{ text: "Start typing here..." }],
+            },
+        ];
+    };
+
+    const editor = InitiatePlateEditor(loadInitialValue());
 
     return (
         <>
