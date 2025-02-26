9650994a95066a7fadb27bb9d05007a09f19ef16
Ichwan Haryo Sembodo <ichwanharyosembodo96@gmail.com>
2025-02-26 18:34:57 +0900

    fix bug in contract page and move to editor page when clicked (#14)

--- frontend/src/pages/contract.tsx
+++ frontend/src/pages/contract.tsx
@@ -1,5 +1,5 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { useContracts, useCurrentDocId } from "../store";
+import { useEffect, useState } from "react";
+import { useCurrentDocId, useContentToShow } from "../store";
 // import DocxImporter from "../components/docxImporter";
 import { useAuth } from "../context/AuthContext";
 
@@ -15,26 +15,14 @@ interface Contract {
 
 interface Contracts {
   contracts: Contract[];
-  setContentToShow: Dispatch<SetStateAction<string>>;  
 }
 
 interface ContractItem {
   contract: Contract;
 }
 
-interface ContractsPageProps {
-  setContentToShow: Dispatch<SetStateAction<string>>;
-}
-
-function ContractsPage({ setContentToShow }: ContractsPageProps) {
+function ContractsPage() {
   const { userInfo } = useAuth();
-  const { updated } = useContracts((state) => state);
-  // const handleCreateNewPage = () => {
-  //   setContentToShow("editor");
-  // };
-
-  console.log("Data Update :", updated);
-
   const [isOpen, setIsOpen] = useState(false);
   const [addOpen, setAddOpen] = useState(false);
   const [file, setFile] = useState<File | null>(null);
@@ -46,10 +34,16 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
   })
 
   const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
+  const closeModal = () => { 
+    setIsOpen(false);
+    fetchContracts();
+  }
 
   const openModalAdd = () => setAddOpen(true);
-  const closeModalAdd = () => setAddOpen(false);
+  const closeModalAdd = () => { 
+    setAddOpen(false);
+    fetchContracts();
+  }
 
   const handleFileChange = (event: any) => {
     setFile(event.target.files[0]);
@@ -106,7 +100,7 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
     event.preventDefault(); // Prevent the default form submission
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/document', {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/document`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -115,56 +109,54 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
         body: JSON.stringify({
           name: baseData.name,
           created_by: userInfo?.displayName,
-          status: 'NEW',
+          status: 'DRAFT',
           version: baseData.version,
           contents: [{"id":"1","type":"p","children":[{"text":"Start typing here..."}]}]
         }), // Send the form data as JSON
       });
 
-      console.log(response)
-
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
 
-      const result = await response.json();
-      console.log('Success:', result);
+      let result = JSON.parse(await response.json());
       useCurrentDocId.setState({ id: result?.document?.id })
-      setContentToShow("editor"); // Set content to show
+      useContentToShow.setState({ content: "editor" }); // Set content to show
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
-  useEffect(() => {
-    const fetchContracts = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:5000/document', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'business-id': 'ichwan@gmail.com'
-          }
-        }); // Adjust the URL as needed
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+  const fetchContracts = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/document`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'business-id': 'ichwan@gmail.com'
         }
-        const data = await response.json();
-        setAllContract(data.map((contract: any) => ({
-          id: contract.id,
-          name: contract.name,
-          language: contract.language,
-          version: contract.version,
-          created: contract.created,
-          updated: contract.updated_content,
-          status: contract.status
-        }))); // Assuming the response contains a 'contracts' array
-      } catch (error) {
-        console.error('Error fetching contracts:', error);
+      }); // Adjust the URL as needed
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    };
+      const data = await response.json();
+      setAllContract(data.map((contract: any) => ({
+        id: contract.id,
+        name: contract.name,
+        language: contract.language || 'EN',
+        version: contract.version,
+        created_at: contract.created_at,
+        updated_at: contract.updated_at,
+        status: contract.status
+      }))); // Assuming the response contains a 'contracts' array
+    } catch (error) {
+      console.error('Error fetching contracts:', error);
+    }
+  };
 
-    fetchContracts(); // Call the function to fetch contracts on component mount
+  // Call fetchContracts on component mount
+  useEffect(() => {
+    fetchContracts();
   }, []); // Empty dependency array to run only on mount
 
   return (
@@ -193,7 +185,7 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
                     className="border px-3 py-2 rounded bg-white"
                 /> */}
       </div>
-      <ContractList contracts={allContract} setContentToShow={setContentToShow} />
+      <ContractList contracts={allContract} />
 
       {isOpen && (
         <div className="fixed inset-0 bg-slate-200 bg-opacity-50 flex items-center justify-center z-50">
@@ -294,7 +286,7 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
                 type="submit"
                 className="bg-green-500 text-white px-4 py-2 rounded-md"
               >
-                Upload
+                Create
               </button>
               <button
                 onClick={closeModalAdd}
@@ -314,55 +306,53 @@ function ContractsPage({ setContentToShow }: ContractsPageProps) {
 export default ContractsPage;
 
 
-function ContractItem(contractItem: any, {setContentToShow}: ContractsPageProps) {
+function ContractItem({ contractItem }: { contractItem: Contract }) {
   const getStatusClass = (status: string) => {
-    switch (status) {
-      case "Drafting":
+    switch (status.toLowerCase()) {
+      case "draft":
         return "bg-blue-100 text-blue-700";
-      case "Reviewing":
+      case "review":
         return "bg-yellow-100 text-yellow-700";
-      case "Finalized":
+      case "final":
         return "bg-green-100 text-green-700";
       default:
         return "";
     }
   };
 
-  console.log("COBA ID: ",contractItem?.contract?.id)
-
   return (
     <tr className="border-b">
       <td className="px-4 py-8 text-gray-700 hover:cursor-pointer" onClick={() => {
-        useCurrentDocId.setState({id: contractItem?.contract?.id});
-        setContentToShow("editor");
-        console.log("ID: asdasdasdasdasd", contractItem?.contract?.id)
-      }}>{contractItem.contract.name}</td>
+        useCurrentDocId.setState({ id: contractItem.id });
+        useContentToShow.setState({ content: "editor" });
+        console.log("ID: asdasdasdasdasd", contractItem.id)
+      }}>{contractItem.name}</td>
       <td className="px-4 py-8 text-gray-700">
-        {contractItem.contract.language ?? 'en'}
+        {contractItem.language ?? 'EN'}
       </td>
       <td className="px-4 py-8 text-gray-700">
-        {contractItem.contract.version}
+        {contractItem.version}
       </td>
       <td className="px-4 py-8 text-gray-700">
-        {contractItem.contract.created_at}
+        {new Date(contractItem.created_at).toLocaleString()}
       </td>
       <td className="px-4 py-8 text-gray-700">
-        {contractItem.contract.updated_at}
+        {new Date(contractItem.updated_at).toLocaleString()}
       </td>
       <td className="px-4 py-8 text-gray-700">
         <span
           className={`px-4 py-2 rounded-3xl ${getStatusClass(
-            contractItem.contract.status
+            contractItem.status
           )}`}
         >
-          {contractItem.contract.status}
+          {contractItem.status}
         </span>
       </td>
     </tr>
   );
 }
 
-function ContractList({ contracts, setContentToShow }: Contracts) {
+function ContractList({ contracts }: Contracts) {
   // const contracts: Contract[] = [
   //     { name: 'Draft Service Agreement Apprvd', language: 'EN', version: 'First draft', created: '5 Aug, 10:02AM', updated: 'Today, 08:00AM', status: 'Uploading' },
   //     { name: 'NDA John Doe', language: 'EN', version: 'Draft v1', created: '5 Aug, 10:02AM', updated: 'Today, 08:00AM', status: 'Drafting' },
@@ -396,7 +386,7 @@ function ContractList({ contracts, setContentToShow }: Contracts) {
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {contracts.map((contract, index) => (
-            <ContractItem key={index} contract={contract} setContentToShow={setContentToShow} />
+            <ContractItem key={index} contractItem={contract} />
           ))}
         </tbody>
       </table>
