eee4d259daea855af5371a113ec7f4d8d5cd4cc9
nurqalik <qaliksulistyo@gmail.com>
2025-03-08 00:48:17 +0700

    Update: add loading spinner

--- frontend/src/components/docxImporter.tsx
+++ frontend/src/components/docxImporter.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { useContentToShow, useContractSelected, useCurrentDocId, useEditorStore } from "../store"; // Import Zustand store
+import {
+  useContentToShow,
+  useContractSelected,
+  useCurrentDocId,
+  useEditorStore,
+} from "../store"; // Import Zustand store
 import * as mammoth from "mammoth";
 import { useAuth } from "../context/AuthContext";
 import { htmlToSlate } from "@slate-serializers/html";
@@ -12,7 +17,7 @@ const DocxImporter = ({ setAllContract }: any) => {
   const [file, setFile] = useState<File | null>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const openModal = () => setIsOpen(true);
   const closeModal = () => {
@@ -54,7 +59,11 @@ const DocxImporter = ({ setAllContract }: any) => {
   };
 
   const handleUpload = async () => {
-    if (!file || file.type !== "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+    if (
+      !file ||
+      file.type !==
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ) {
       alert("Only .docx files are accepted!");
       return;
     }
@@ -73,33 +82,37 @@ const DocxImporter = ({ setAllContract }: any) => {
     };
     reader.readAsArrayBuffer(file);
 
-    if(content.length === 0) {
-      alert("Please wait for a moment, upload document is still in progress...");
+    if (content.length === 0) {
+      alert(
+        "Please wait for a moment, upload document is still in progress..."
+      );
       return;
     }
 
-    setIsLoading(true)
+    setIsLoading(true);
 
     try {
       if (!userInfo?.email) {
         alert("Please login!");
-        return navigate('/');
+        return navigate("/");
       }
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/document`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "business-id": userInfo?.email,
-        },
-        body: JSON.stringify({
-          name: file?.name,
-          created_by: userInfo?.email,
-          status: "DRAFT",
-          version: "1",
-          contents: content,
-        }), // Send the form data as JSON
-      });
-
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/document`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "business-id": userInfo?.email,
+          },
+          body: JSON.stringify({
+            name: file?.name,
+            created_by: userInfo?.email,
+            status: "DRAFT",
+            version: "1",
+            contents: content,
+          }), // Send the form data as JSON
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -114,9 +127,10 @@ const DocxImporter = ({ setAllContract }: any) => {
         status: result?.document?.status,
         version: result?.document?.version,
       });
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (error) {
       console.error("Error:", error);
+      setIsLoading(false);
     }
   };
 
@@ -147,7 +161,7 @@ const DocxImporter = ({ setAllContract }: any) => {
     try {
       if (!userInfo?.email) {
         alert("Please login!");
-        return navigate('/');
+        return navigate("/");
       }
 
       const response = await fetch(
@@ -246,7 +260,26 @@ const DocxImporter = ({ setAllContract }: any) => {
                 disabled={isLoading}
                 className="bg-green-500 text-white px-4 py-2 rounded-md mr-2 disabled:cursor-not-allowed"
               >
-                Upload
+                {isLoading ? (
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-6 h-6 text-gray-400 animate-spin dark:text-gray-600 fill-white dark:fill-gray-300"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  "Upload"
+                )}
               </button>
               <button
                 onClick={closeModal}
