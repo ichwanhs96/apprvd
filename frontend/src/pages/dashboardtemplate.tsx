deb9e6b2ecac054b88843bd3c908d27a3649a24f
nurqalik <qaliksulistyo@gmail.com>
2025-04-16 02:55:30 +0700

    Update: Initial branch

---
+++ frontend/src/pages/dashboardtemplate.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import DashboardNavbar from "../components/navbar/dashboard";
+import Sidebar from "../components/sidebar";
+import MainContainer from "../components/mainContainer";
+import { NavItem } from "../components/navbar/dashboard";
+import EditorPage from "./editor";
+import ContractPage from "./contract";
+import { useContentToShow } from "../store"
+import { Bounce, ToastContainer } from "react-toastify";
+
+const DashboardTemplate: React.FC = () => {
+  const navItems: NavItem[] = [];
+
+  const content = useContentToShow((state) => state.content);
+
+  const getContentToShow = () => {
+    switch (content.toLowerCase()) {
+      case 'editorTemplate':
+        return EditorPage;
+      case 'contractsTemplate':
+        return ContractPage;
+      default:
+        return ContractPage;
+    }
+  }
+
+  return (
+    <>
+      <DashboardNavbar navItems={navItems}></DashboardNavbar>
+      <Sidebar></Sidebar>
+      <MainContainer content={getContentToShow()}></MainContainer>
+      <ToastContainer transition={Bounce} />
+    </>
+  );
+};
+
+export default DashboardTemplate;
